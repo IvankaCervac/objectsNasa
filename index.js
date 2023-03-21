3074004e7bda799410ded5c8ebf1739d66729a53
IvankaCervac <cervac.ivanka@gmail.com>
2023-03-21 20:33:07 +0200

    am adaugat o functie !isOnMap

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* NASA va trimite pe Marte mai mulți roboți ce vor explora planeta.
+/*PRIMA PARTE
+ NASA va trimite pe Marte mai mulți roboți ce vor explora planeta.
  Aceștia vor naviga o suprafață dreptunghiulară pentru a colecta 
  diferite date ce vor fi transmise înapoi pe Pământ.
 
@@ -30,45 +31,88 @@ Output așteptat:
 
 1 3 N */
 
+/* A DOUA PARTE EXTENSIE
+Dacă un roboțel primește o comandă ce îl face să iasă 
+de pe hartă, toate comenzile ulterioare vor fi ignorate,
+ iar la final va fi adăugat textul RIP după
+coordonatele sale înaintea ieșirii de pe hartă.
+Exemplu input:
+5 5
+1 5 N
+LMMRMLMRM
+Output:
+0 5 V RIP
+*/
 
+function giveCordinates (x, y, orientation, instructioToMove, size ) {
+	let cordinates = [],
+		[sizeXMax, sizeYMax] = size;
+	for(let  letter of instructioToMove) {
+			if (letter === 'L') {
+				orientation = turnLeft( orientation)
+			} else if (letter === 'R') {
+				orientation = turnRight(orientation);	
+			} else if (letter == 'M') {
+                if(!isOnMap(x, y, sizeXMax, sizeYMax, orientation)){
+                    return [x, y, orientation, "RIP"]
+                }else {
+                [x, y] = moveForward(x, y, orientation)  
+                }
+			}
+        }		
+	}
+	     //if (x > sizeXMax || y > sizeYMax) {
+		//return `RIP`
+	    //}
 
-let robot = {
-    X : 1,
-    Y : 2,
-    currentDirection : "N",
-    instructionL(){
-        if (this.currentDirection === "N"){
-        return this.currentDirection.replace("N", "V")
-        }else if(this.currentDirection === "E"){
-            return this.currentDirection.replace("E", "N")
-    }else if(this.currentDirection === "S"){
-        return this.currentDirection.replace("S", "E")  
-    }else if(this.currentDirection === "V"){
-        return this.currentDirection.replace("V", "S")  
-}
-}
-    instructionR(){
-    if (this.currentDirection === "N"){
-    return this.currentDirection.replace("N", "E")
-    }else if(this.currentDirection === "E"){
-        return this.currentDirection.replace("E", "S")
-}else if(this.currentDirection === "S"){
-    return this.currentDirection.replace("S", "V")  
-}else if(this.currentDirection === "V"){
-    return this.currentDirection.replace("V", "N")  
-}
-}
-    instructionM(){
-    if (this.currentDirection === "N"){
-    return this.Y(Y++)
-    }else if(this.currentDirection === "E"){
-        return this.currentDirection.replace("E", "S")
-}else if(this.currentDirection === "S"){
-    return this.currentDirection.replace("S", "V")  
-}else if(this.currentDirection === "V"){
-    return this.currentDirection.replace("V", "N")  
-}
-}
-}
+	cordinates.push(x, y, orientation);
+	return cordinates
 
-console.log(robot.currentDirection);
+    function turnLeft(orientation) {
+			if (orientation == "N") {
+				orientation = 'V'
+			} else if (orientation == "V") {
+				orientation = 'S'
+			} else if (orientation == "S") {
+				orientation = 'E'
+			}  else if (orientation == "E") {
+				orientation = 'N'
+			} 
+	    return orientation
+    }
+    function turnRight(orientation) {
+		if (orientation == "N") {
+			orientation = 'E'
+		} else if (orientation == "E") {
+			orientation = 'S'
+		} else if (orientation == "S") {
+			orientation = 'V'
+		}  else if (orientation == "V") {
+			orientation = 'N'
+		} 
+	    return orientation
+    }
+    function isOnMap(x, y, sizeXMax, sizeYMax, orientation){
+        if ((x == sizeXMax && orientation == "E")||
+        ( x == 0 && orientation =="V") || 
+        ( y == sizeYMax && orientation == "N") ||
+        ( y == 0 && orientation =="S")){
+            return false
+        }
+        return true
+    }
+
+    function moveForward(x, y, orientation) {
+	    if (orientation == 'N') {
+		y++
+	    } else if (orientation == 'S') {
+		y--
+	    } else if (orientation == 'E') {
+		x++
+	    } else if (orientation == 'V') {
+		x--
+	    }
+	    return [x, y];
+    }
+
+console.log(giveCordinates(1, 5, 'N', 'LMMRMLMRM', [5, 5]));
